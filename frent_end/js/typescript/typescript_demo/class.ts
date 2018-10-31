6c000ab5bf99f92d15800601d6752b78a0039946
liuying <liuying@yryz.com>
2018-10-31 16:31:31 +0800

    class

--- frent_end/js/typescript/typescript_demo/class.ts
+++ frent_end/js/typescript/typescript_demo/class.ts
@@ -2,12 +2,52 @@
 abstract class SuperClass {
 
 	private _versions: number = 1.0;
+	private _events = {};
 
-	getVersions() {
+	getVersions():number {
 		return this._versions;
 	}
 
-	protected exchangeSort(collections: Array<number | string>, orderBy: boolean = true): void {
+	getEvents():any {
+		return this._events;
+	}
+
+	on(name: string, callback: () => void):void {
+		if (!this._events[name]) {
+			(this._events[name] as Array<() => void>) = [callback];
+		} else {
+			this._events[name].push();
+		}
+	}
+
+	emit(name: string, ...args:Array<any>):void {	
+		if (this._events[name]) {
+			let events = this._events[name];
+			events.forEach((callback) => {
+				callback(...args)
+			});
+		}
+	}
+
+	removeListen(name: string):void {
+		this._events[name] = [];
+	}
+
+	/**
+	 * 检查空指针异常
+	 * @param value 
+	 * return true | false
+	 */
+ 	protected checkNullPointer(value: any): boolean {
+		return typeof value !== 'undefined'; 
+	}
+
+	/**
+	 * 交换排序
+	 * @param collections Array
+	 * @param orderBy boolean
+	 */
+	exchangeSort(collections: Array<number | string>, orderBy: boolean = true): void {
 		for (let i = 0; i < collections.length; i++) {
 			for (let j = 0; j < i; j++) {
 				let ascend = orderBy ? collections[j] > collections[i] : collections[j] < collections[i];
@@ -19,6 +59,8 @@ abstract class SuperClass {
 			}
 		}
 	}
+
+
 
 	abstract removeByIndexs<T>(collections: T[], indexs: number[]): T[];
 
@@ -47,9 +89,9 @@ class Utils extends SuperClass {
 	removeByValues<T, U extends T>(collections: T[], values: Array<U>, key?: string): T[] {
 		let list = [...collections];
 		for (var i = 0; i < values.length; i++) {
-			let value = typeof key !== 'undefined' ?  values[i][key] : values[i];
+			let value = this.checkNullPointer(key) ? values[i][key] : values[i];
 			for (var j = 0; j < list.length; j++) {
-				let item = typeof key !== 'undefined' ? list[i][key] : list[i];
+				let item = this.checkNullPointer(key) ? list[i][key] : list[i];
 				if (value === item) {
 					list.splice(j, 1);
 					j--;
@@ -59,7 +101,7 @@ class Utils extends SuperClass {
 		return list;
 	}
 
-	findIndexByAttr<T, U extends T>(collections: T[], key: string, item: U): number {
+	getIndexByAttr<T, U>(collections: T[], key: string, item: U): number {
 		for (let i = 0; i < collections.length; i++) {
 			let object = collections[i];
 			if (object[key] === item[key]) {
@@ -68,9 +110,7 @@ class Utils extends SuperClass {
 		}
 	}
 
-	
 
-	
 }
 
 
@@ -86,8 +126,9 @@ interface testType2 {
 }
 
 
-let util = new Utils();
+let util:Utils = new Utils();
 let remaining = util.removeByIndexs<number>([1, 2, 3], [2]);
 util.findIndexByAttr<testType, testType2>([{ name: 'zhangsan', age: 18 }, { name: 'zhangsan', age: 18 }], "", { name: 'ss', age: 18 });
 let values = util.removeByValues<string, string>(['1','2','3'], ['1', '2']);
+util.removeByIndexs<string>(['1', '2'], []);
 console.log(values);
